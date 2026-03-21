333d48e69200cce48d7ee4142f0e4df9e3662d37
albxrtx <albertomariannechita@gmail.com>
2026-03-21 16:25:23 +0100

    sprinboot inicializado

---
+++ backend/concurrent-seat-booking/src/main/java/com/example/concurrent_seat_booking/ConcurrentSeatBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrent_seat_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentSeatBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentSeatBookingApplication.class, args);
+	}
+
+}
---
+++ backend/concurrent-seat-booking/src/test/java/com/example/concurrent_seat_booking/ConcurrentSeatBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrent_seat_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentSeatBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
